a658aa7bbac7ce055b7b0584d7eac8ed12acd739
RatneshNamdev <ratnesh.mygithub@gmail.com>
2022-03-09 04:32:38 +0530

    Initial Commit: Selenium first code by manual added jar and making it chrome launcher...

---
+++ src/com/liseinfotech/selenium/chromeLaunch/SeleniumTest.java
@@ -0,0 +1,20 @@
+package com.liseinfotech.selenium.chromeLaunch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/aaa/Downloads/chromedriver");
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("http://yahoo.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+
+
+    }
+}
